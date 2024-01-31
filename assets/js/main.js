dfcfd304a2cec0f69721d09d52eead9d8fb3a186
jiyeon Han <chiaki1221@naver.com>
2024-01-31 15:42:17 +0900

    전체수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,6 +13,24 @@ gsap.ticker.add(time => {
 
 gsap.ticker.lagSmoothing(0);
 
+// nav클릭시, 해당 섹션으로 이동
+const navItem = document.querySelectorAll('.nav-item');
+navItem.forEach(li => {
+    li.addEventListener('click', function (e) {
+        e.preventDefault();
+        id = this.dataset.id;
+
+        this.classList.add('on');
+        navItem.forEach(other => {
+            if (other !== this) {
+                other.classList.remove('on');
+            }
+        });
+
+        lenis.scrollTo(id);
+    });
+});
+
 /* gsap */
 // 로딩 카운터 모션
 const items = document.querySelector('.num .data');
@@ -36,199 +54,160 @@ const loadingMotion = gsap.timeline({
 
     onComplete: function () {
         // 끝나고 bg모션 진행
-        // introMotion.play();
+        introMotion.play();
+        navItemMotion.play();
     },
 });
 loadingMotion.to('.loading', {
-    'clip-path': 'polygon(0 0, 100% 0, 100% 0, 0 0)',
+    background: 'rgb(104, 0, 255)',
 });
+loadingMotion.to(
+    '.loading',
+    {
+        'clip-path': 'polygon(0 0, 100% 0, 100% 0, 0 0)',
+    },
+    '-=0.5',
+);
 
 // 메인bg 움직임
-// const introMotion = gsap.timeline({
-//     defaults:{
-//         delay:1,
-//         opacity:0,
-//         duration:1,
-//     },
-//     // paused:true,
-// })
-// introMotion.from('.holographic-bg img:nth-child(1)',{xPercent:-3,yPercent:3},'a')
-// introMotion.from('.holographic-bg img:nth-child(2)',{xPercent:2,yPercent:0},'a')
-// introMotion.from('.holographic-bg img:nth-child(3)',{xPercent:4,yPercent:-4},'a')
-// introMotion.from('.holographic-bg img:nth-child(4)',{xPercent:-1,yPercent:3},'a')
-// introMotion.from('.holographic-bg img:nth-child(5)',{xPercent:3,yPercent:2},'a')
+const introMotion = gsap.timeline({
+    defaults: {
+        delay: 1,
+        opacity: 0,
+        duration: 1,
+    },
+    paused: true,
+});
+introMotion.from('.holographic-bg img:nth-child(1)', { xPercent: -3, yPercent: 3 }, 'a');
+introMotion.from('.holographic-bg img:nth-child(2)', { xPercent: 2, yPercent: 0 }, 'a');
+introMotion.from('.holographic-bg img:nth-child(3)', { xPercent: 4, yPercent: -4 }, 'a');
+introMotion.from('.holographic-bg img:nth-child(4)', { xPercent: -1, yPercent: 3 }, 'a');
+introMotion.from('.holographic-bg img:nth-child(5)', { xPercent: 3, yPercent: 2 }, 'a');
 
-gsap.to('.holographic-bg img', {
+// nav-item 모션
+let navItemMotion = gsap.timeline({
+    default: {
+        delay: 0.5,
+    },
+    paused: true,
+});
+
+navItemMotion.from('.nav-item a', {
+    y: -15,
     opacity: 0,
-    scale: 1.2,
-    rotate: '45deg',
+    duration: 1,
+    stagger: 0.2,
+});
+navItemMotion.from('.sc-main .title em', {
+    y: -20,
+    opacity: 0,
+    duration: 0.7,
+    stagger: 0.3,
+});
+
+// contact영역 미세 움직임
+gsap.from('.sc-contact .inner', {
     scrollTrigger: {
-        trigger: '.content',
-        start: 'top 0%',
-        scrub: 1,
+        trigger: '.sc-contact',
+        start: '0% 100%',
+        end: '100% 100%',
+        scrub: 0,
         // markers: true,
     },
+    yPercent: -30,
 });
 
-let time2 = gsap.timeline();
-time2.set('.nav-item', {
-    y: -30,
-    opacity: 0,
-});
-time2.to('.nav-item', {
-    stagger: 0.3,
-    y: 0,
-    opacity: 1,
-    ease: Expo.easeInOut,
-    delay: 0.5,
-});
-time2.from('.util-area', {
-    y: 50,
-    opacity: 0,
-    ease: Expo.easeInOut,
-});
-time2.to(
-    '.nav-list',
-    1,
-    {
-        backgroundSize: '100%',
-        ease: Expo.easeInOut,
-    },
-    '-=0.5',
-);
-time2.to('.nav-list', 1, {
-    backgroundSize: '0%',
-    ease: Expo.easeInOut,
-});
-time2.pause();
-
-gsap.to('.sc-about .bg-box', {
-    yPercent: 10,
-    scrollTrigger: {
-        trigger: '.sc-about',
-        start: 'top 80%',
-        end: 'bottom 0%',
-        scrub: 1,
-        // markers: true,
-    },
-});
-
-let txtUp = gsap.from('.sc-about .txt-up', {
-    stagger: 0.3,
-    y: 30,
-    opacity: 0,
-});
-txtUp.pause();
+// 영역별 nav on활성
 ScrollTrigger.create({
-    trigger: '.sc-about',
-    start: 'top 30%',
+    trigger: '.sc-main',
+    start: '0% 0%',
+    onEnterBack: function () {
+        navItem.forEach(li => li.classList.remove('on'));
+        navItem[0].classList.add('on');
+    },
+});
+ScrollTrigger.create({
+    trigger: '.sc-intro',
+    start: '0% 10%',
     // markers: true,
-    onEnter: () => {
-        txtUp.play();
+    onEnter: function () {
+        navItem.forEach(li => li.classList.remove('on'));
+        navItem[1].classList.add('on');
     },
-    onLeaveBack: () => {
-        txtUp.reverse();
-    },
-});
-
-gsap.from('.sc-about .stack-loop', {
-    xPercent: 10,
-    scrollTrigger: {
-        trigger: '.sc-about',
-        start: '20% 30%',
-        scrub: 1,
-        // markers: true,
+    onLeaveBack: function () {
+        navItem.forEach(li => li.classList.remove('on'));
+        navItem[0].classList.add('on');
     },
 });
-
-let time3 = gsap.timeline();
-time3.from('.sc-contact .desc', {
-    x: -100,
-    opacity: 0,
+ScrollTrigger.create({
+    trigger: '.sc-project',
+    start: '0% 10%',
+    // markers: true,
+    onEnter: function () {
+        navItem.forEach(li => li.classList.remove('on'));
+        navItem[2].classList.add('on');
+    },
+    onLeaveBack: function () {
+        navItem.forEach(li => li.classList.remove('on'));
+        navItem[1].classList.add('on');
+    },
 });
-time3.from(
-    '.sc-contact .contact .left > *',
-    {
-        x: -100,
-        opacity: 0,
-        stagger: 0.1,
-    },
-    '-=0.2',
-);
-time3.from(
-    '.sc-contact .contact .right > *',
-    {
-        x: -100,
-        opacity: 0,
-        stagger: 0.1,
-    },
-    '-=0.5',
-);
-time3.pause();
 ScrollTrigger.create({
     trigger: '.sc-contact',
-    start: '0% 70%',
+    start: '0% 10%',
     // markers: true,
-    onEnter: () => {
-        time3.play();
+    onEnter: function () {
+        navItem.forEach(li => li.classList.remove('on'));
+        navItem[3].classList.add('on');
     },
-    onLeaveBack: () => {
-        time3.reverse();
+    onLeaveBack: function () {
+        navItem.forEach(li => li.classList.remove('on'));
+        navItem[2].classList.add('on');
     },
 });
+
+// headline
+// const introTl = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: '.sc-intro',
+//         start: '0% 70%',
+//         // markers: true,
+//     },
+// });
+// introTl.from('.sc-intro .headline', 0.3, {
+//     y: -20,
+//     opacity: 0,
+// });
+// introTl.from('.sc-intro .desc', 0.3, {
+//     y: -20,
+//     opacity: 0,
+// });
+
+// gsap.from('.sc-project .headline', {
+//     y: -20,
+//     opacity: 0,
+//     scrollTrigger: {
+//         trigger: '.sc-project',
+//         start: '0% 70%',
+//         markers: true,
+//     },
+// });
+// gsap.from('.sc-subprj .headline', {
+//     y: -20,
+//     opacity: 0,
+//     scrollTrigger: {
+//         trigger: '.sc-subprj',
+//         start: '0% 70%',
+//         markers: true,
+//     },
+// });
 
 /* index */
-// gnb메뉴 toggle 버튼
-const btnMenu = document.querySelector('.btn-menu');
-const gnb = document.querySelector('.gnb');
-btnMenu.addEventListener('click', function () {
-    this.classList.toggle('on');
-    gnb.classList.toggle('open');
-    document.querySelector('html').classList.toggle('hidden');
-
-    if (gnb.classList.contains('open')) {
-        time2.restart();
-    }
-});
-
-const secTop1 = document.querySelector('.sc-intro').offsetTop;
-const secTop2 = document.querySelector('.sc-project').offsetTop;
-const secTop3 = document.querySelector('.sc-about').offsetTop;
-const secTop4 = document.querySelector('.sc-contact').offsetTop;
-// console.log(secTop1, secTop2, secTop3, secTop4);
-
-const navListAll = document.querySelectorAll('.nav-list a');
-navListAll.forEach((list, i) => {
-    list.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        btnMenu.classList.remove('on');
-        gnb.classList.remove('open');
-        document.querySelector('html').classList.remove('hidden');
-
-        if (i == 0) {
-            window.scrollTo({ top: secTop1, behavior: 'smooth' });
-        } else if (i == 1) {
-            window.scrollTo({ top: secTop2, behavior: 'smooth' });
-        } else if (i == 2) {
-            window.scrollTo({ top: secTop3, behavior: 'smooth' });
-        } else if (i == 3) {
-            window.scrollTo({ top: secTop4, behavior: 'smooth' });
-        }
-    });
-});
-
 // 스크롤시, header
 let lastScroll = 0;
 window.addEventListener('scroll', function () {
     const header = document.querySelector('.header');
-    // console.log(this.scrollY);
     current = this.scrollY;
-    if (current == 0) {
-        header.style.backgroundColor = 'transparent';
-    } else {
-        header.style.backgroundColor = 'rgba(28, 28, 28, 0.9)';
-    }
 
     if (current > lastScroll) {
         header.classList.add('sticky');
@@ -239,7 +218,7 @@ window.addEventListener('scroll', function () {
 });
 
 // gnb menu hover effect
-let textEl = document.querySelectorAll('.gnb .text');
+let textEl = document.querySelectorAll('.nav-item a');
 textEl.forEach(el => {
     let innerText = el.innerText; //텍스트 지우기전에 변수에 담아둠
     el.innerHTML = '';
@@ -265,60 +244,37 @@ textEl.forEach(el => {
     });
 });
 
-// 프로젝트 로고이미지 마우스 움직임
-let hoverImage = document.querySelectorAll('.prj-item .img-box');
-function showImage(e) {
-    for (let i = 0; i < hoverImage.length; i++) {
-        hoverImage[i].style.left = e.pageX + 'px';
-        hoverImage[i].style.top = e.pageY + 'px';
-    }
-}
-const cursor = document.querySelector('.cursor');
-const prjSec = document.querySelectorAll('.sc-project .prj-item');
-// document.addEventListener('mousemove', showImage);
-prjSec.forEach(prj => {
-    prj.addEventListener('mousemove', showImage);
-    prj.addEventListener('mouseover', function () {
-        cursor.style.display = 'none';
-    });
-    prj.addEventListener('mouseleave', function () {
-        cursor.style.display = 'block';
+// cursor point커스텀
+document.addEventListener('mousemove', function (e) {
+    xVal = e.clientX;
+    yVal = e.clientY;
+
+    gsap.to('.cursor', 0.3, {
+        x: xVal,
+        y: yVal,
     });
 });
 
-function mousePointEvent() {
-    // cursor point커스텀
-    const cursor = document.querySelector('.cursor');
-    document.addEventListener('mousemove', e => {
-        cursor.style.left = e.pageX + 'px';
-        cursor.style.top = e.pageY + 'px';
+// 특정영역에서 cursor point 확대
+const cursor = document.querySelector('.cursor');
+const allLink = document.querySelectorAll('a');
+allLink.forEach(a => {
+    a.addEventListener('mouseover', function () {
+        cursor.classList.add('on');
     });
-    // 특정영역에서 cursor point 확대
-    const allElements = document.querySelectorAll('*');
-    let pointTxt = Array.from(allElements).filter(function (el) {
-        return el.dataset.hover === 'on';
+    a.addEventListener('mouseleave', function () {
+        cursor.classList.remove('on');
     });
-    pointTxt.forEach(txt => {
-        txt.addEventListener('mouseover', function () {
-            cursor.classList.add('on');
-        });
-        txt.addEventListener('mouseleave', function () {
-            cursor.classList.remove('on');
-        });
-    });
-    const allLink = document.querySelectorAll('a');
-    allLink.forEach(a => {
-        a.addEventListener('mouseover', function () {
-            cursor.classList.add('on');
-        });
-        a.addEventListener('mouseleave', function () {
-            cursor.classList.remove('on');
-        });
-    });
-}
+});
 
-if (window.matchMedia('(min-width: 1024px)').matches) {
-    mousePointEvent();
-} else {
-    console.log('size not pc');
+// current time
+const clock = document.querySelector('.sc-main .date');
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
+getClock();
+setInterval(getClock, 1000);
